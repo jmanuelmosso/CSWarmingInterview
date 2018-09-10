6daf34ce58b53727bb947c755d28e98859b90bfa
Manuel Mosso <jmanuel.mosso@icloud.com>
2018-09-10 15:34:12 -0500

    Initial Commit

---
+++ CSWarmingInterview/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CSWarmingInterview
+//
+//  Created by Manuel Mosso on 9/10/18.
+//  Copyright © 2018 Manuel Mosso. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
